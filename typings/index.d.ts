514d7ba1506deec2d13c38023f32ba6457b9950a
Nay <59286251+purplnay@users.noreply.github.com>
2020-10-03 23:15:27 +0200

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,14 +1,38 @@
 declare module 'purpl-logger' {
+
+  /**
+   * The path to the file to add logs to.
+   */
+  const path: string
+
+  /**
+   * Whether to prepend the new log or not.
+   */
+  const prepend: boolean
+
+  /**
+   * Prepend logs to a file.
+   * @param message The log message.
+   * @param file The filepath where to log the message to.
+   * @param prepend Whether to prepend the new log or not.
+   * @return The full log message.
+   */
+  const log: (message: string, file?: string, prepend?: boolean) => string
+
+  /**
+    * A logger that date and prepend logs to files.
+    */
   type Logger = {
+
     /**
-     * The path to the file to add logs to.
-     */
+   * The path to the file to add logs to.
+   */
     path: string,
 
     /**
      * Whether to prepend the new log or not.
      */
-    prepend: boolean;
+    prepend: string,
 
     /**
      * Prepend logs to a file.
@@ -18,12 +42,15 @@ declare module 'purpl-logger' {
      * @return The full log message.
      */
     log(message: string, file?: string, prepend?: boolean): string
-  };
+  }
 
-  /**
-  * A logger that date and prepend logs to files.
-  */
-  const logger: Logger;
+  const logger: Logger
 
-  export default logger;
+  export {
+    path,
+    prepend,
+    log
+  }
+
+  export default logger
 }
